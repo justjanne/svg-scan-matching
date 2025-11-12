cebc68beaa35eee5985befa5fe28e1804dc15997
Janne Mareike Koschinski <janne@kuschku.de>
2025-11-12 22:01:32 +0100

    Implement registration mark detection feature

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from scanalign-integrated!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ tests/test_detection.py
@@ -0,0 +1,10 @@
+import os
+
+from detect.detection import detect_registration_marks
+
+
+def test_detect_registration_marks():
+    test_dir = "tests/resources"
+    files = sorted(os.listdir(test_dir))
+    for file in files:
+        detect_registration_marks(os.path.join(test_dir, file), "build/test")
