67678d5d90d3f0ded1512f76baf1ec3bf53d3758
Janne Mareike Koschinski <mail@justjanne.de>
2025-11-13 15:48:27 +0100

    Sort test resources by type

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -4,7 +4,7 @@ from detect.detection import detect_registration_marks
 
 
 def test_detect_registration_marks():
-    test_dir = "tests/resources"
+    test_dir = "tests/resources/scan"
     files = sorted(os.listdir(test_dir))
     for file in files:
         detect_registration_marks(os.path.join(test_dir, file), "build/test")
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -7,7 +7,7 @@ from transform.util import patch_viewbox
 patch_viewbox()
 
 def test_detect_registration_marks():
-    test_dir = "tests/svg"
+    test_dir = "tests/resources/svg"
     files = sorted(os.listdir(test_dir))
     for file in files:
         parse_svg(os.path.join(test_dir, file), "build/test")
