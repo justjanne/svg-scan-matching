0058ada372ec01f12060dd012acca9437448c44a
Janne Mareike Koschinski <janne@kuschku.de>
2026-03-22 18:34:56 +0100

    Apply new detection logic

---
+++ align/detection.py
@@ -0,0 +1,95 @@
+import os.path
+
+import cv2
+
+
+def detect_registration_marks(file: str, debug_dir: str | None = None):
+    im_scan = cv2.imread(file)
+    im_scan = cv2.resize(im_scan, (3504, im_scan.shape[0] * 3504 // im_scan.shape[1]))
+
+    write_debug_image(im_scan, file, "_scan", debug_dir)
+
+    im_threshold = preprocess_image(im_scan)
+    write_debug_image(im_threshold, file, "_threshold", debug_dir)
+
+    contours, hierarchy = cv2.findContours(im_threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    candidates = [c for c in contours if match_contour(c)]
+    marks = [process_contour(c) for c in candidates]
+
+    if debug_dir is not None:
+        im_contours = im_scan
+        im_contours = cv2.drawContours(im_contours, contours, -1, (255, 0, 0), 3)
+        im_contours = cv2.drawContours(im_contours, candidates, -1, (0, 0, 255), 3)
+        for mark in marks:
+            im_contours = cv2.circle(im_contours, mark, 2, (0, 255, 0), 2)
+            im_contours = cv2.circle(im_contours, mark, 60, (0, 255, 0), 2)
+        write_debug_image(im_contours, file, "_contours", debug_dir)
+
+    return [
+        to_percent(im_scan, mark)
+        for mark in marks
+    ]
+
+def to_percent(scan: cv2.typing.MatLike, point: (float, float)) -> (float, float):
+    x, y = point
+    height, width, _ = scan.shape
+    return x / width, y / height
+
+
+def preprocess_image(im_scan: cv2.typing.MatLike) -> cv2.typing.MatLike:
+    im_hsl = cv2.cvtColor(im_scan, cv2.COLOR_BGR2HSV)
+    im_saturation = im_hsl[:, :, 1]
+    _, im_saturation = cv2.threshold(im_saturation, 60, 255, cv2.THRESH_BINARY)
+    kernel_saturation = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    im_saturation = cv2.morphologyEx(im_saturation, cv2.MORPH_ERODE, kernel_saturation, iterations=2)
+    im_saturation = cv2.morphologyEx(im_saturation, cv2.MORPH_DILATE, kernel_saturation, iterations=3)
+
+    im_grayscale = cv2.cvtColor(im_scan, cv2.COLOR_BGR2GRAY)
+    im_grayscale = cv2.bitwise_not(im_grayscale)
+
+    kernel_denoise = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    _, im_denoise = cv2.threshold(im_grayscale, 63, 255, cv2.THRESH_BINARY)
+    im_denoise = cv2.morphologyEx(im_denoise, cv2.MORPH_OPEN, kernel_denoise)
+
+    im_grayscale = cv2.bitwise_and(im_grayscale, im_denoise)
+    im_grayscale = cv2.bitwise_and(im_grayscale, cv2.bitwise_not(im_saturation))
+
+    _, im_threshold = cv2.threshold(im_grayscale, 127, 255, cv2.THRESH_BINARY)
+    im_threshold = cv2.morphologyEx(im_threshold, cv2.MORPH_DILATE, kernel_denoise, iterations=3)
+    return im_threshold
+
+
+def process_contour(contour: cv2.typing.MatLike) -> cv2.typing.Point:
+    center, size, angle = cv2.minAreaRect(contour)
+    center_x, center_y = center
+    return [int(center_x), int(center_y)]
+
+
+def write_debug_image(image: cv2.typing.MatLike, file: str, suffix: str, debug_dir: str | None):
+    if debug_dir is not None:
+        os.makedirs(debug_dir, exist_ok=True)
+        basename, extension = os.path.splitext(os.path.basename(file))
+        cv2.imwrite(os.path.join(debug_dir, f"{basename}{suffix}{extension}"), image)
+
+
+def match_contour(contour: cv2.typing.MatLike) -> bool:
+    _, radius = cv2.minEnclosingCircle(contour)
+
+    area = cv2.contourArea(contour) / radius / radius
+    perimeter = cv2.arcLength(contour, True) / radius
+
+    hull = cv2.convexHull(contour)
+    hull_area = cv2.contourArea(hull) / radius / radius
+    hull_perimeter = cv2.arcLength(hull, True) / radius
+
+    match_area = match_value(area, 1.0, .16)
+    match_hull_area = match_value(hull_area, 2.26, .08)
+    match_perimeter = match_value(perimeter, 7.78, .06)
+    match_hull_perimeter = match_value(hull_perimeter, 5.78, .05)
+    match_radius = match_value(radius, 64, 0.1)
+
+    return match_radius and match_hull_perimeter and match_perimeter and match_area and match_hull_area
+
+
+def match_value(actual: float, expected: float, deviation: float) -> bool:
+    return abs(actual - expected) < (expected * deviation)
--- align/main.py
+++ align/main.py
@@ -7,53 +7,7 @@ import os
 import cv2
 import numpy
 
-DEBUGPATH = os.getenv("FCM_DEBUG_PATH")
-
-def close_enough(a, b):
-    return min(a, b) / max(a, b) > 0.7
-
-
-def find_contours(image: cv2.typing.MatLike) -> Sequence[cv2.typing.MatLike]:
-    image_grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image_grayscale = cv2.bitwise_not(image_grayscale)
-    ret, thresh = cv2.threshold(image_grayscale, 60, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
-
-
-def prefilter_double(shape: numpy.shape, contour: cv2.typing.MatLike) -> bool:
-    area = cv2.contourArea(contour)
-    _, _, w, h = cv2.boundingRect(contour)
-    contour_length = cv2.arcLength(contour, True)
-    if 0 in [w, h, area, contour_length]:
-        return False
-    if not close_enough(contour_length, w * 2.2 + h * 2.2):
-        return False
-    if not close_enough(w, h):
-        return False
-    if not close_enough(area, w * 11):
-        return False
-    if not close_enough(w, shape[1] / 30):
-        return False
-    return True
-
-
-def prefilter_single(shape: numpy.shape, contour: cv2.typing.MatLike) -> bool:
-    area = cv2.contourArea(contour)
-    _, _, w, h = cv2.boundingRect(contour)
-    contour_length = cv2.arcLength(contour, True)
-    if 0 in [w, h, area, contour_length]:
-        return False
-    if not close_enough(contour_length, w * 2.2 + h * 2.2):
-        return False
-    if not close_enough(w, h):
-        return False
-    if not close_enough(area, w * 11):
-        return False
-    if not close_enough(w, shape[1] / 60):
-        return False
-    return True
-
+import detection
 
 def distance(point1, point2):
     x1, y1 = point1
@@ -61,184 +15,28 @@ def distance(point1, point2):
     return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y2 - y1, 2))
 
 
-def marks_match(mark1, mark2):
-    for point1 in mark1:
-        for point2 in mark2:
-            if distance(point1, point2) < 5:
-                return [point1, point2]
-    return None
-
-
-def find_center(points, index):
-    return sorted(point[index] for point in points)[1:3]
-
-
-def merge_cluster(cluster: Sequence[numpy.array]) -> numpy.array:
-    for el1 in cluster:
-        for el2 in cluster:
-            if numpy.array_equal(el1, el2):
-                continue
-            match = marks_match(el1, el2)
-            if match is None:
-                continue
-            return cv2.boxPoints(cv2.minAreaRect(numpy.array(el1 + el2)))
-    return None
-
-
-def merge_clusters(candidates: list[list[(float, float)]]):
-    xs = [point[0] for el in candidates for point in el]
-    ys = [point[1] for el in candidates for point in el]
-    if len(xs) == 0 or len(ys) == 0:
-        return []
-
-    min_x = min(xs)
-    max_x = max(xs)
-    min_y = min(ys)
-    max_y = max(ys)
-    threshold_x = (min_x + max_x) / 2
-    threshold_y = (min_y + max_y) / 2
-
-    top_left = [el for el in candidates
-                if all(point[0] < threshold_x for point in el)
-                and all(point[1] < threshold_y for point in el)]
-    top_right = [el for el in candidates
-                 if all(point[0] > threshold_x for point in el)
-                 and all(point[1] < threshold_y for point in el)]
-    bottom_left = [el for el in candidates
-                   if all(point[0] < threshold_x for point in el)
-                   and all(point[1] > threshold_y for point in el)]
-    bottom_right = [el for el in candidates
-                    if all(point[0] > threshold_x for point in el)
-                    and all(point[1] > threshold_y for point in el)]
-
-    marks = [
-        merge_cluster(top_left),
-        merge_cluster(top_right),
-        merge_cluster(bottom_left),
-        merge_cluster(bottom_right),
-    ]
-
-    return [mark for mark in marks if mark is not None]
-
-def distance_mark(center: (float, float), mark: ((float, float), (float, float))):
-    ((x1, x2), (y1, y2)) = mark
-    distances = [
-        distance((x1, y1), center),
-        distance((x2, y1), center),
-        distance((x1, y2), center),
-        distance((x2, y2), center),
-    ]
-    return sum(distances) / len(distances)
-
-
-def sort_marks(marks: list[list[(float, float)]]) -> list[((float, float), (float, float))]:
-    min_x = min(x1 for ((x1, _), (_, _)) in marks)
-    max_x = max(x2 for ((_, x2), (_, _)) in marks)
-    min_y = min(y1 for ((_, _), (y1, _)) in marks)
-    max_y = max(y2 for ((_, _), (_, y2)) in marks)
+def sort_marks(marks: list[(float, float)]) -> list[(float, float)]:
+    min_x = min(x for (x, _) in marks)
+    max_x = max(x for (x, _) in marks)
+    min_y = min(y for (_, y) in marks)
+    max_y = max(y for (_, y) in marks)
     center_x = (min_x + max_x) / 2
     center_y = (min_y + max_y) / 2
     center = (center_x, center_y)
 
-    distances = [distance_mark(center, mark) for mark in marks]
+    distances = [distance(center, mark) for mark in marks]
     distances.sort()
     median_distance = distances[len(distances) // 2]
 
-    marks.sort(key=lambda mark: abs(median_distance - distance_mark((center_x, center_y), mark)))
+    marks.sort(key=lambda mark: abs(median_distance - distance((center_x, center_y), mark)))
     marks = marks[0:4]
 
-    top_left = next(((x1, x2), (y1, y2)) for ((x1, x2), (y1, y2)) in marks if x2 < center_x and y2 < center_y)
-    top_right = next(((x1, x2), (y1, y2)) for ((x1, x2), (y1, y2)) in marks if x1 > center_x and y2 < center_y)
-    bottom_left = next(((x1, x2), (y1, y2)) for ((x1, x2), (y1, y2)) in marks if x2 < center_x and y1 > center_y)
-    bottom_right = next(((x1, x2), (y1, y2)) for ((x1, x2), (y1, y2)) in marks if x1 > center_x and y1 > center_y)
+    top_left = next((x, y) for (x, y) in marks if x < center_x and y < center_y)
+    top_right = next((x, y) for (x, y) in marks if x > center_x and y < center_y)
+    bottom_left = next((x, y) for (x, y) in marks if x < center_x and y > center_y)
+    bottom_right = next((x, y) for (x, y) in marks if x > center_x and y > center_y)
 
     return [top_left, top_right, bottom_left, bottom_right]
-
-
-def process_candidates(candidates: list[list[(float, float)]]) -> list[(float, float)]:
-    return [
-        (find_center(cnt, 0), find_center(cnt, 1))
-        for cnt in candidates
-    ]
-
-
-def to_mm(shape: numpy.shape, point: (float, float)) -> (float, float):
-    x, y = point
-    height, width, _ = shape
-    return x / width, y / height
-
-
-def avg(*items: list[float]) -> float:
-    if len(items) == 0:
-        return 0.0
-    return sum(items) / len(items)
-
-
-def simplify_single(contour: numpy.array) -> numpy.array:
-    _, points = cv2.minEnclosingTriangle(contour)
-    return [(point[0][0], point[0][1]) for point in points]
-
-
-def simplify_double(contour: numpy.array) -> numpy.array:
-    rect = cv2.minAreaRect(contour)
-    points = cv2.boxPoints(rect)
-    return points
-
-
-def add_border(image: cv2.typing.MatLike):
-    for y in range(0, image.shape[0]):
-        image[y, 0] = (255, 255, 255)
-    for x in range(0, image.shape[1]):
-        image[0, x] = (255, 255, 255)
-    for y in range(0, image.shape[0]):
-        image[y, image.shape[1] - 1] = (255, 255, 255)
-    for x in range(0, image.shape[1]):
-        image[image.shape[0] - 1, x] = (255, 255, 255)
-
-
-def find_marks(file: str) -> [(float, float), (float, float), (float, float), (float, float)]:
-    scan = cv2.imread(file)
-    if DEBUGPATH != "":
-        dbgScan = cv2.cvtColor(scan, cv2.COLOR_BGR2GRAY)
-        dbgScan = cv2.cvtColor(dbgScan, cv2.COLOR_GRAY2RGB)
-
-    add_border(scan)
-    candidates = find_contours(scan)
-    contours_single = [cnt for cnt in candidates if prefilter_single(scan.shape, cnt)]
-    contours_double = [cnt for cnt in candidates if prefilter_double(scan.shape, cnt)]
-    shapes_single = [simplify_single(cnt) for cnt in contours_single]
-    shapes_double = [simplify_double(cnt) for cnt in contours_double]
-
-    if DEBUGPATH is not None:
-        dbg = dbgScan.copy()
-        cv2.drawContours(dbg, contours_single, -1, (0, 0, 255), 2)
-        cv2.drawContours(dbg, contours_double, -1, (255, 0, 0), 2)
-        for tri in shapes_single:
-            cv2.drawContours(dbg, [numpy.intp(tri)], -1, (0, 255, 255), 2)
-        for rect in shapes_double:
-            cv2.drawContours(dbg, [numpy.intp(rect)], -1, (0, 255, 255), 2)
-        cv2.imwrite(DEBUGPATH+"/contours_1.jpg", dbg)
-
-    marks = process_candidates(shapes_double + merge_clusters(shapes_single))
-
-    if DEBUGPATH is not None:
-        dbg = dbgScan.copy()
-        for ((x1, x2), (y1, y2)) in marks:
-            cv2.rectangle(dbg, numpy.intp((x1, y1)), numpy.intp((x2, y2)), (0, 255, 0), 2)
-        cv2.imwrite(DEBUGPATH+"/contours_2.jpg", dbg)
-
-    marks = sort_marks(marks)
-
-    if DEBUGPATH is not None:
-        dbg = dbgScan.copy()
-        for ((x1, x2), (y1, y2)) in marks:
-            cv2.rectangle(dbg, numpy.intp((x1, y1)), numpy.intp((x2, y2)), (0, 255, 0), 2)
-        cv2.imwrite(DEBUGPATH + "/contours_3.jpg", dbg)
-
-    return [
-        to_mm(scan.shape, (avg(x1, x2), avg(y1, y2)))
-        for ((x1, x2), (y1, y2)) in marks
-    ]
 
 
 def apply_matrix(
@@ -281,7 +79,7 @@ def generate_cut(source: str, target: str, transform: (float, float, float, floa
 
 
 def process_cut(source: str, scan: str, out: str):
-    marks = find_marks(scan)
+    marks = sort_marks(detection.detect_registration_marks(scan))
     transform = calculate_transform(marks)
     generate_cut(source, out, transform)
 
